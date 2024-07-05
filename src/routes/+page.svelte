f64d8c25e12a495899bbbb74908000cb855a4a96
= <=>
2024-07-05 18:18:46 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        console.log("This is a +page");
+    });
+</script>
+
+<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
+
+<style lang="postcss">
+    :global(body) {
+        background-color: theme('colors.gray.100');
+        padding: theme('spacing.4');
+    }
+</style>
